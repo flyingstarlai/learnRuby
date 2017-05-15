029972b8c72d3ed3b557f4e04954cb6a455955ed
izayoi <flyingstarlai@gmail.com>
2017-05-15 16:53:56 +0800

    finish basic

--- basic/string.rb
+++ basic/string.rb
@@ -12,6 +12,7 @@
 
 # name = "aloha"
 
+# p name.capitalize
 # p name.upcase
 # p name.downcase
 # p name.swapcase
@@ -26,6 +27,47 @@ str = "  cat is cute animal, be like cat  "
 
 # p str.strip
 # p str.split
-p str.split.size
-p str.strip.split(//)
+# p str.split.size
+# p str.strip.split(//)
+
+# words = <<MLS
+
+#     Disuatu tempat nan jauh
+#     di dunia nusantara.
+
+#     Ga ada apa-apa
+#     sih.
+
+# MLS
+
+# puts words
+
+# puts 'good bye "My Love"'
+# puts "good bye \"My Love\""
+
+# kata = "Hello world"
+# puts "#{kata}"
+# puts '#{kata}'
+
+fname = "hairy"
+lname = "potter"
+
+# p fname << lname << "wizard"
+# p fname.concat(lname)
+# p fname.prepend(lname)
+
+story = "budi ke pasar membeli sayur"
+
+p story.length
+# p story[3]
+# p story[3, story.length]
+p story[3..5]
+p story[3...5]
+
+# story[0] ="r"
+story[8..12] = "toko"
+p story
+
+p story.include?("sayur")
+
 
